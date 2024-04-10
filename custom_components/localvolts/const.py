9cb45da53ef0b89f63805f9da5abe94a4c6304cc
JonathanXing <jonathan.xing17@gmail.com>
2024-04-10 22:02:27 +1000

    Update const.py

--- custom_components/localvolts/const.py
+++ custom_components/localvolts/const.py
@@ -1,1 +1,11 @@
+# Sensors
+earningsflexup = 'Feed_in_price'
+costsflexup = 'Cost_price'
 
+SENSOR_DESCRIPTIONS = {
+    earningsflexup: SensorEntityDescription(
+        name='earningsflexup',
+    ),
+    costsflexup: SensorEntityDescription(
+        name='costsflexup',
+    ),
